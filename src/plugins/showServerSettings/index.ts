3b41b11e275a251edf3de3949f54945ab305b31d
cootshk <cootshk@duck.com>
2025-06-02 17:23:11 -0500

    debug logging

--- src/plugins/showServerSettings/index.ts
+++ src/plugins/showServerSettings/index.ts
@@ -9,6 +9,8 @@ import definePlugin from "@utils/types";
 import { PermissionsBits, PermissionStore } from "@webpack/common";
 import { Channel } from "discord-types/general";
 
+const DEBUG_LOGGING = false;
+
 const perms_list = [
     // Show the server settings button
     "canAccessGuildSettings",
@@ -58,11 +60,10 @@ export default definePlugin({
             replacement: {
                 // yes, this breaks on any if statement.
                 // yes, i'm too lazy to fix it.
-                match: /\}isRoleHigher\(((\i,)*\i?)\)\{(.*?)}(\i)\(/,
+                match: /\}isRoleHigher\(((\i,)*\i?)\)\{([^}]*)\}(\i)\(/,
                 // replace: "}isRoleHigher($1){$3}$4("
-                replace: "}isRoleHigher($1){let args=[$1]; if (this.can($self.PermissionsBits.MANAGE_ROLES,args[0])) { return false; };$3}$4(",
                 // replace: (args, _, code, nextfunc) => `}isRoleHigher(${args}){let args=[${args}]; if (can(PermissionsBits.MANAGE_ROLES,args[0])) { return false; };${code}}${nextfunc}(`,
-                // replace: (args, _, code, nextfunc) => `}isRoleHigher(${args}){ if (isRoleHigher(${args}) { return false; };${code}}${nextfunc}(`,
+                replace: "}isRoleHigher($1) { if (this.can($self.PermissionsBits.MANAGE_ROLES,arguments[0])) {return false;}; $3}$4(",
             }
         },
     ],
@@ -80,11 +81,11 @@ export default definePlugin({
     */
 
     can: (name, ...args: any[]) => {
-        console.log(`Args passed to can(${name}):`, args);
+        if (DEBUG_LOGGING) console.log(`Args passed to can(${name}):`, args);
         const check = permissionCheck(args[0], args[1], args.slice(2));
-        console.log("Check: ", check);
+        if (DEBUG_LOGGING) console.log("Check: ", check);
         // breakpoint; // Uncomment this line to pause execution in a debugger
-        if (!args.includes(undefined)) {
+        if (DEBUG_LOGGING && !args.includes(undefined)) {
             throw new Error("This is a test error to see if the plugin is working.");
         }
         return check;
@@ -94,34 +95,34 @@ export default definePlugin({
     PermissionsBits: PermissionsBits
 
 });
-const MANAGE_PERMS = [
-    "ADMINISTRATOR",
-    "MANAGE_CHANNELS",
-    "MANAGE_EVENTS",
-    "MANAGE_GUILD",
-    "MANAGE_GUILD_EXPRESSIONS",
-    "MANAGE_MESSAGES",
-    "MANAGE_NICKNAMES",
-    "MANAGE_ROLES",
-    "MANAGE_THREADS",
-    "MANAGE_WEBHOOKS",
-];
+// const MANAGE_PERMS = [
+//     "ADMINISTRATOR",
+//     "MANAGE_CHANNELS",
+//     "MANAGE_EVENTS",
+//     "MANAGE_GUILD",
+//     "MANAGE_GUILD_EXPRESSIONS",
+//     "MANAGE_MESSAGES",
+//     "MANAGE_NICKNAMES",
+//     "MANAGE_ROLES",
+//     "MANAGE_THREADS",
+//     "MANAGE_WEBHOOKS",
+// ];
 function permissionCheck(permissionBits: bigint, object: Channel, ..._) {
     // Check for the permission bits
     const max_bit = Math.ceil(Math.log2(Number(permissionBits)));
     const permission_bits: bigint[] = []; // list of permissions found in PermissionsBits
 
-    for (let i = 0; i <= max_bit; i++) {
-        if (permissionBits & (1n << BigInt(i))) {
-            permission_bits.push(1n << BigInt(i));
+    for (let i = 0n; i <= max_bit; i++) {
+        if (permissionBits & (1n << i)) {
+            permission_bits.push(1n << i);
         }
     }
-    console.log("Permission bits found:", permission_bits);
+    if (DEBUG_LOGGING) console.log("Permission bits found:", permission_bits);
 
-    console.log(`Checking permissions: ${permissionBits} on object:`, object);
+    if (DEBUG_LOGGING) console.log(`Checking permissions: ${permissionBits} on object:`, object);
     for (const [name, bits] of Object.entries(PermissionsBits)) {
         if (permission_bits.includes(bits)) {
-            console.log(`Permission ${name} (${bits}) is being checked.`);
+            if (DEBUG_LOGGING) console.log(`Permission ${name} (${bits}) is being checked.`);
             if (name.includes("MANAGE") || name === "ADMINISTRATOR") { return true; }
         }
     }
